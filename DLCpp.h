2fc3a3c808c2db7d3438b736842be21ad071a153
Yurick <yurick.hauschild@gmail.com>
2014-05-14 21:58:09 -0300

    Finalizada a criação dos atributos das classes Field. Falta implementar os métodos para chaining e as classes Model.

--- DLCpp.h
+++ DLCpp.h
@@ -17,9 +17,82 @@ namespace models
 		bool __unique;
 	};
 
+	class BigIntegerField : public Field
+	{
+		signed long long __default; 
+	};
+
 	class BooleanField : public Field
 	{
 		bool __default;
+	};
+
+	class CharField : public Field
+	{
+		string __default;
+		int __max_length;
+	};
+
+	class FloatField : public Field
+	{
+		float __default;
+	};
+
+	class IntegerField : public Field
+	{
+		int __default;
+	};
+
+	class PositiveIntegerField : public Field
+	{
+		unsigned int __default;
+	};
+
+	class PositiveSmallIntegerField : public Field
+	{
+		unsigned short int __default;
+	};
+
+	class SmallIntegerField : public Field
+	{
+		short int __default;
+	};
+
+	class TextField : public Field
+	{
+		string __default;
+	};
+
+	class ForeignKey : public Field
+	{
+		string __related_name;
+		Field& __to_field;
+		string __on_delete;
+
+	public:
+		ForeignKey(const &Model);
+	};
+
+	class ManyToManyField : public Field
+	{
+		string __related_name;
+		bool __symmetrical;
+		Model& __through;
+		Field& __through_field[2];
+		string __db_table;
+
+	public:
+		ManyToManyField(const &Model);
+	};
+
+	class OneToOneField : public Field
+	{
+		string __related_name;
+		Field& __to_field;
+		string __on_delete;
+
+	public:
+		OneToOneField(const &Model);
 	};
 }
 
